1af62fa26f4ff2258758e2235dd46129e5df8fa9
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2019-12-07 12:47:34 -0800

    Add files via upload

---
+++ Background.java
@@ -0,0 +1,221 @@
+//imports
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+import java.io.*;
+import java.io.File.*;
+import java.awt.Desktop.*;
+
+public class Background extends JPanel implements MouseListener{
+    JFrame frame;
+    JScrollPane scroller;
+    JButton backbutton;
+    String path;
+    String temppath;
+    File currentFile;
+    JPopupMenu rcwindowm;
+    JMenuItem newfile;
+    JMenuItem newfolder;
+    String OS;
+    public Background(){
+        //initializeds jcomponents and variables
+        frame = new JFrame("Daniel's Explorer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700,500);
+
+        backbutton = new JButton("Back");
+        backbutton.addActionListener(new BackButtonListener());
+
+        rcwindowm = new JPopupMenu();
+        newfile = new JMenuItem("New .txt File");
+        newfolder = new JMenuItem("New Folder");
+        rcwindowm.add(newfile);
+        rcwindowm.add(newfolder);
+        newfile.addMouseListener(new NewFileListener());
+        newfolder.addMouseListener(new NewFolderListener());
+
+        scroller = new JScrollPane(this);
+        //checks operating systema and sets path accordingly for windows and mac   
+        OS = System.getProperty("os.name");
+        if(OS.startsWith("Windows")){
+            path = "C:/";
+        }
+        if(OS.startsWith("Mac")){
+            path = "/";
+        }
+        
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.addMouseListener(this);
+
+        refreshBrowser();
+
+        frame.add(scroller);
+        frame.add(BorderLayout.SOUTH, backbutton);
+        frame.setVisible(true);
+    }
+
+    public void refreshBrowser(){
+        //resets the window
+        this.removeAll();
+        currentFile = new File(path);
+        //prints the files in the folder as buttons
+        for(String s: currentFile.list()){
+            JButton b = new JButton(s);
+            b.addActionListener(new ButtonListeners(b));
+            this.add(b);
+        }
+        this.revalidate();
+        frame.repaint();
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e){
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e){
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e){
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e){
+        //shows the right click menu
+        if(e.getButton() == MouseEvent.BUTTON3){
+            rcwindowm.show(this, e.getX(), e.getY());
+        }
+        if(e.getButton() == MouseEvent.BUTTON1 && rcwindowm.isVisible()){
+            rcwindowm.setVisible(false);
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e){
+    }
+
+    public static void main(String[] args){
+        Background b = new Background();
+    }
+
+    class ButtonListeners implements ActionListener{
+        String buttontext;
+        public ButtonListeners(JButton b){
+            buttontext = b.getText();
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent a){
+            temppath = path;
+            path = path + buttontext + "/";
+            File checkFile = new File(path);
+            //checks if the file can be opened then shows an error if it cannot otherwise shows that file
+            if(checkFile.isFile()){
+                if(Desktop.isDesktopSupported()){
+                    Desktop d = Desktop.getDesktop();
+                    try{
+                        d.open(checkFile);
+                        path = temppath;
+                    }catch(IOException e){
+                        JOptionPane.showMessageDialog(null, "Error, this file cannot be opened");
+                        path = temppath;
+                        refreshBrowser();
+                    }
+                }
+            }
+            //tells the user if they do not have accses to the file
+            try{
+                refreshBrowser();
+            }catch(NullPointerException e){
+                JOptionPane.showMessageDialog(null, "error, you cant access that");
+                path = temppath;
+                refreshBrowser();
+            }
+        }
+    }
+    //button listener for the back button which uses the get parentfile method to go back
+    class BackButtonListener implements ActionListener{
+        @Override 
+        public void actionPerformed(ActionEvent a){
+            try{
+                temppath = path;
+                path = currentFile.getParentFile() + "/";
+                refreshBrowser();
+            }catch(NullPointerException e){
+                JOptionPane.showMessageDialog(null, "You are Already in the Root Directory!");
+                path = temppath;
+                refreshBrowser();
+            }
+        }
+    }
+    //listener to create the folder
+    class NewFolderListener implements MouseListener{
+        @Override
+        public void mouseExited(MouseEvent e){
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e){
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e){
+        }
+        //checks if the new folder name is alllowed
+        @Override
+        public void mousePressed(MouseEvent e){
+            rcwindowm.setVisible(false);
+            String nfname = JOptionPane.showInputDialog(frame, "Input New Folder Name:", null);
+            if(nfname!=null && nfname.trim().length()!=0){
+                new File(path+"/"+nfname).mkdirs();
+
+                refreshBrowser();
+            }
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e){
+        }
+    }
+    class NewFileListener implements MouseListener{
+        @Override
+        public void mouseExited(MouseEvent e){
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e){
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e){
+        }
+        //mouse listener which checks if the new file name is allowed
+        @Override
+        public void mousePressed(MouseEvent e){
+            rcwindowm.setVisible(false);
+            String nfname = JOptionPane.showInputDialog(frame, "Input New File Name:", null);
+            if(nfname!=null && nfname.trim().length()!=0){
+                if(nfname.length()<4){
+                    nfname = nfname + ".txt";
+                }else{
+                    if(!nfname.substring(nfname.length()-4).equals(".txt")){
+                        nfname = nfname + ".txt";
+                    }
+                }
+                File newfile = new File(path + "/" + nfname);
+                try{
+                    PrintWriter out = new PrintWriter(newfile);
+                }catch(FileNotFoundException fnfe){
+                }
+
+                refreshBrowser();
+            }
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e){
+        }
+    }
+}
